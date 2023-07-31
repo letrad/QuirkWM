103368488806ba599493feebd41dabb65647822b
Letrad <letrad@disroot.org>
2023-07-31 13:46:28 +1000

    things roughly work

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 typedef struct WindowNode {
     Window window;
@@ -18,21 +19,41 @@ int gap = 10;
 // Variable to store the last focused window
 Window last_focused_window = None;
 
-void spawn_kitty() {
+void spawn_program(const char *program) {
     if (fork() == 0) {
-        execlp("kitty", "kitty", NULL);
-        fprintf(stderr, "Failed to launch kitty\n");
-        exit(1);
+        // Child process
+        if (fork() == 0) {
+            // Grandchild process
+            setsid(); // Create a new session
+            execlp(program, program, NULL);
+            perror("Failed to launch program");
+            exit(1);
+        }
+        exit(0); // Exit child process
     }
+
+    // Parent process waits for the child to exit
+    wait(NULL);
 }
+
 
 void add_window(Window w) {
     WindowNode *new_node = malloc(sizeof(WindowNode));
     new_node->window = w;
-    new_node->next = window_list;
-    window_list = new_node;
+    new_node->next = NULL;
+
+    if (window_list == NULL) {
+        window_list = new_node;
+    } else {
+        WindowNode *current = window_list;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = new_node;
+    }
     num_windows++;
 }
+
 
 void remove_window(Window w) {
     WindowNode **current = &window_list;
@@ -49,19 +70,38 @@ void remove_window(Window w) {
 }
 
 void tile_windows() {
+    if (num_windows == 0) return;
+
+    // If only one window, make it occupy the entire screen, with a gap around it
+    if (num_windows == 1) {
+        XMoveResizeWindow(dpy, window_list->window, gap, gap, DisplayWidth(dpy, 0) - 2 * gap, DisplayHeight(dpy, 0) - 2 * gap);
+        return;
+    }
+
     WindowNode *node = window_list;
-    int i = 0;
     int screen_width = DisplayWidth(dpy, 0);
     int screen_height = DisplayHeight(dpy, 0);
-    int window_width = screen_width / num_windows;
-    while (node != NULL) {
-        int x = i * (window_width + gap);
-        int y = 0;
-        int width = window_width - gap;
-        int height = screen_height - 2 * gap;
-        XMoveResizeWindow(dpy, node->window, x, y + gap, width, height);
+    int master_width = (screen_width - gap) / 2; // 50% for the master window
+    int stack_width = (screen_width - master_width - gap * (num_windows - 1)) / (num_windows - 1);
+
+    // Master window
+    if (node != NULL) {
+        XMoveResizeWindow(dpy, node->window, gap, gap, master_width - gap, screen_height - 2 * gap);
         node = node->next;
-        i++;
+    }
+
+    // Stack windows
+    for (int i = 1; node != NULL; i++) {
+        int x = master_width + gap * i + stack_width * (i - 1);
+        int y = gap;
+        int width = stack_width;
+        int height = screen_height - 2 * gap;
+        XMoveResizeWindow(dpy, node->window, x, y, width, height);
+        node = node->next;
+    }
+    // Set focus to the last focused window
+    if (last_focused_window != None) {
+        XSetInputFocus(dpy, last_focused_window, RevertToParent, CurrentTime);
     }
 }
 
@@ -139,7 +179,7 @@ int main(void) {
             }
                 // Super + T pressed
             else if (keysym == XK_t && (ev.xkey.state & Mod4Mask)) {
-                spawn_kitty();
+                spawn_program("kitty");
             }
                 // Arrow keys pressed (with Mod4 key)
             else if (keysym == XK_Left && (ev.xkey.state & Mod4Mask)) {
@@ -147,12 +187,15 @@ int main(void) {
             } else if (keysym == XK_Right && (ev.xkey.state & Mod4Mask)) {
                 focus_next_window();
             }
-        } else if (ev.xany.type == MapRequest) {
+        }
+        else if (ev.xany.type == MapRequest) {
             XMapRequestEvent *e = &ev.xmaprequest;
+            printf("MapRequest event received, window ID: %lu\n", e->window);
             add_window(e->window);
             XMapWindow(dpy, e->window);
             set_last_focused_window(e->window);
             tile_windows();
+
         } else if (ev.xany.type == UnmapNotify) {
             // Handling window unmap
             XUnmapEvent *e = &ev.xunmap;
